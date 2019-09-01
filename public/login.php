0d319bc2ebdab5fc7e8281c60440f7771853d5e5
zetovicmarko1 <zetovicmarko1@gmail.com>
2019-09-01 21:05:57 +1000

    login fix

--- public/login.php
+++ public/login.php
@@ -8,11 +8,11 @@
     <header>
         <h1 class="text-center">Login</h1>
         <h2 class="text-center bg-warning"><?php display_message(); ?></h2>
-        <div class="col-sm-4 col-sm-offset-5">
+        <div class="col-sm-4 col-sm-offset-4">
             <form class="" action="" method="post" enctype="multipart/form-data">
 
                 <div class="form-group"><label for="">
-                        username<input type="text" name="username" class="form-control"></label>
+                        User Name<input type="text" name="username" class="form-control"></label>
                 </div>
                 <div class="form-group"><label for="password">
                         Password<input type="password" name="password" class="form-control"></label>
